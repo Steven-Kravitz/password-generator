8f5af39c3517adf5be07722cd003710f20e1d8f3
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-01-22 19:23:51 -0500

    Finished the script for all password character options. Wrapped main code in if else statement to ensure password is between 8-128 characters

--- Develop/script.js
+++ Develop/script.js
@@ -31,36 +31,107 @@ function generatePassword() {
   var noUpper = letterSetLower.concat(numberSet, specialSet);
   var noLower = letterSetUpper.concat(numberSet, specialSet);
   var password = "";
+if (length >= "8" && length <= "128") {
 
+
+  // All True
 if (useUpper === true && useLower === true && useNumbers === true && useSpecial === true) {
   for (var i = 0, n =allChar.length; i < length; i++) {
     password += allChar.charAt(Math.random() * n);
 }
 
     return password;
+
+  // No Upper Case, Rest True
 } else if (useUpper === false && useLower === true && useNumbers === true && useSpecial === true) {
   for (var i = 0, n =noUpper.length; i < length; i++) {
     password += noUpper.charAt(Math.random() * n);
 }
 
   return password;
+  // No Lower Case, Rest True
 } else if (useUpper === true && useLower === false && useNumbers === true && useSpecial === true) {
   for (var i = 0, n =noLower.length; i < length; i++) {
     password += noLower.charAt(Math.random() * n);
 }
 
   return password;
+  // No Numbers, Rest True
+} else if (useUpper === true && useLower === true && useNumbers === false && useSpecial === true) {
+  for (var i = 0, n =noNumber.length; i < length; i++) {
+    password += noNumber.charAt(Math.random() * n);
+}
+
+  return password;
+  // No Specials, Rest True
+} else if (useUpper === true && useLower === true && useNumbers === true && useSpecial === false) {
+  for (var i = 0, n =noSpecial.length; i < length; i++) {
+    password += noSpecial.charAt(Math.random() * n);
+}
+
+  return password;
+  // No Upper, No Numbers
+} else if (useUpper === false && useLower === true && useNumbers === false && useSpecial === true) {
+  for (var i = 0, n =letterSetLower.concat(specialSet).length; i < length; i++) {
+    password += letterSetLower.concat(specialSet).charAt(Math.random() * n);
+}
+
+  return password;
+  // No Upper, No Special
+}else if (useUpper === false && useLower === true && useNumbers === true && useSpecial === false) {
+  for (var i = 0, n =letterSetLower.concat(numberSet).length; i < length; i++) {
+    password += letterSetLower.concat(numberSet).charAt(Math.random() * n);
+}
+
+  return password;
+  // Only Lower
+}else if (useUpper === false && useLower === true && useNumbers === false && useSpecial === false) {
+  for (var i = 0, n =letterSetLower.length; i < length; i++) {
+    password += letterSetLower.charAt(Math.random() * n);
+}
+
+  return password;
+  // No Lower, No Number
+}else if (useUpper === true && useLower === false && useNumbers === false && useSpecial === true) {
+  for (var i = 0, n =letterSetUpper.concat(specialSet).length; i < length; i++) {
+    password += letterSetUpper.concat(specialSet).charAt(Math.random() * n);
+}
+
+  return password;
+  // No Lower, No Special
+}else if (useUpper === true && useLower === false && useNumbers === true && useSpecial === false) {
+  for (var i = 0, n =letterSetUpper.concat(numberSet).length; i < length; i++) {
+    password += letterSetUpper.concat(numberSet).charAt(Math.random() * n);
+}
+
+  return password;
+  // Only Upper
+}else if (useUpper === true && useLower === false && useNumbers === false && useSpecial === false) {
+  for (var i = 0, n =letterSetUpper.length; i < length; i++) {
+    password += letterSetUpper.charAt(Math.random() * n);
+}
+
+  return password;
+  // No Specials or Numbers
+}else if (useUpper === true && useLower === true && useNumbers === false && useSpecial === false) {
+  for (var i = 0, n =allLetter.length; i < length; i++) {
+    password += allLetter.charAt(Math.random() * n);
+}
+
+  return password;
+  // Close loop
+} else {
+  return "No valid selections made. Try again."
+}
+} else {
+  alert("Please set your password length between 8-128 characters")
+  return "Please set your password length between 8-128 characters"
 }
 
 
-
-
-
-
-
-
 }
-
+// }
+// Framework for if else statements in generatePassword function
   // if(){
 
   // }
